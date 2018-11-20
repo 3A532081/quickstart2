fd2523f886d758ec425c96dca57375265eed30dc
3A532081 <tanshui601@gmail.com>
2018-11-20 20:42:40 +0800

    在TaskController內新增一個destroy函數，用以刪除使用者某一個task (有授權之下)

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -32,4 +32,12 @@ class TaskController extends Controller
         ]);
         return redirect('/tasks');
     }
+    public function destroy(Request $request, Task $task)
+    {
+        $this->authorize('destroy', $task);
+
+        $task->delete();
+
+        return redirect('/tasks');
+    }
 }
